dc42fd4f09032e6edfcdb46b44b3cfc6d4059284
armantorkzaban <armantorkzaban@icloud.com>
2025-12-15 00:31:15 +0100

    feat: Replace logo with Iran Nation branding

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,22 +1,15 @@
-import LogoDark from "@/assets/logo-dark.svg";
-import LogoLight from "@/assets/logo-light.svg";
 import { env } from "../env";
+
+// Iran Nation logo from forum
+const LOGO_URL = "https://forum.irannation.com/api/v4/image/04e00544-4bed-46fc-8db8-901814ada24a.png";
 
 export function Logo() {
   return (
-    <>
-      <img
-        src={LogoDark}
-        style={{ height: 38, width: 90 }}
-        className="hidden dark:block object-contain object-left"
-        alt={`${env.REACT_APP_NAME} logo`}
-      />
-      <img
-        src={LogoLight}
-        style={{ height: 38, width: 90 }}
-        className="dark:hidden object-contain object-left"
-        alt={`${env.REACT_APP_NAME} logo`}
-      />
-    </>
+    <img
+      src={LOGO_URL}
+      style={{ height: 38, width: 90 }}
+      className="object-contain object-left"
+      alt={`${env.REACT_APP_NAME} logo`}
+    />
   );
 }
